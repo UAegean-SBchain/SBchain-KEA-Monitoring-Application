1fa338ec22dda5200bcba9ee02d89779440f632f
Mibburo <c.bitsikas@gmail.com>
2020-09-21 10:58:48 +0300

    clean up

--- src/main/java/com/example/ethereumserviceapp/controller/EthereumController.java
+++ src/main/java/com/example/ethereumserviceapp/controller/EthereumController.java
@@ -4,17 +4,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import com.example.ethereumserviceapp.model.Case;
-import com.example.ethereumserviceapp.model.CredsAndExp;
-import com.example.ethereumserviceapp.model.State;
 import com.example.ethereumserviceapp.service.EthereumService;
 import com.example.ethereumserviceapp.service.MongoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -45,52 +41,5 @@ public class EthereumController {
         ethService.addCase(monitoredCase);
                 
     }
-
-    // @PostMapping("/updateCase")
-    // protected void updateCase(@RequestParam(value = "uuid", required = true) String uuid, @RequestParam(value = "caseName", required = true) String caseName,
-    //         @RequestParam(value = "isStudent", required = true) Boolean isStudent,
-    //         @RequestParam(value = "date", required = true) String date,
-    //         @RequestParam(value = "state", required = true) String state) {
-
-    //     Case monitoredCase = new Case();
-    //     monitoredCase.setUuid(uuid);
-    //     monitoredCase.setName(caseName);
-    //     monitoredCase.setIsStudent(isStudent);
-
-    //     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-    //     //LocalDateTime dateTime = LocalDateTime.parse(str, formatter);
-
-    //     monitoredCase.setDate(LocalDateTime.parse(date, formatter));
-    //     monitoredCase.setState(State.valueOf(state));
-
-    //     ethService.updateCase(monitoredCase);
-
-    //     //contractService.updateCase(uuid, caseName, isStudent, date, state);
-    // }
-
-    // @PostMapping("/deployContract")
-    // protected void deployContract() {
-    //     contractService.deployContract();
-    // }
-
-    // @GetMapping("/getAllCases")
-    // protected String getAllCases() {
-    //     return ethService.getAllCaseUUID().get(0).trim();
-    //     //contractService.getAllCases();
-    // }
-
-    // @GetMapping("/getCase")
-    // protected Case getCase(@RequestParam(value = "uuid", required = true) String uuid) {
-    //     //Optional<Case> theCase = ethService.getCaseByUUID(uuid);
-    //     return ethService.getCaseByUUID(uuid).get();
-    // }
-
-    // @GetMapping("/getCreds")
-    // public @ResponseBody
-    // CredsAndExp[] getAllCredentialIds(@RequestParam(value = "uuid", required = true) String uuid) {
-
-    //     return mongoServ.findCredentialIdsByUuid(uuid);
-
-    // }
 
 }
